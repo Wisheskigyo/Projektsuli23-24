6695c2cd19e4a9a14cabee1bc2f2250295701490
heged <leventehegedus33@gmail.com>
2023-12-07 12:23:21 +0100

    asdfg

---
+++ Weboldal/teszt.js
@@ -0,0 +1,7 @@
+Text
+/* font-family: 'Kaushan Script', cursive; */
+/* font-family: 'Pinyon Script', cursive; */
+/* font-family: 'Petit Formal Script', cursive; */
+/* font-family: 'Aguafina Script', cursive; */
+/* font-family: 'Rouge Script', cursive; */
+// font-family: 'Roboto', sans-serif;
