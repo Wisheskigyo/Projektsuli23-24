8acae6f4b6ae5e2357b0302ed2e911ba49bab3f4
heged <leventehegedus33@gmail.com>
2023-12-14 18:02:47 +0100

    Merfoldko2.1.4

--- Weboldal/registration.js
+++ Weboldal/registration.js
@@ -1,23 +1,32 @@
-function validateForm() {
+function validateForm(event) {
+    event.preventDefault();
+
     const userType = document.getElementById("userType").value;
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirmPassword").value;
 
     if (password !== confirmPassword) {
         alert("Passwords do not match!");
-        return false;
+    } else {
+        alert("Registration successful! Redirecting to fooldal.html");
+        window.location.href = "fooldal.html";
     }
+}
 
-    // Additional validation logic can be added here
+document.getElementById("registrationForm").addEventListener("submit", validateForm);
+
+
+
+// Additional validation logic can be added here
 
     // Assuming the validation is successful, redirect to "fooldal.html"
-    alert("Registration successful! Redirecting to fooldal.html");
+
 
 
 
     // If you want to prevent the default form submission, uncomment the line below
     // return false;
-}
+
 
 
 document.getElementById("userType").addEventListener("change", function () {
