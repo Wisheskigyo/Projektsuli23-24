544ceb22bd57f5c0951cdc12ca035c837b11e6a5
heged <leventehegedus33@gmail.com>
2023-12-18 10:29:51 +0100

    Merfoldko2.1.6

--- Weboldal/registration.js
+++ Weboldal/registration.js
@@ -1,40 +1,72 @@
-function validateForm(event) {
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
+
+nextBtnFirst.addEventListener("click", function(event){
     event.preventDefault();
+    slidePage.style.marginLeft = "-25%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+nextBtnSec.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "-50%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+nextBtnThird.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "-75%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+submitBtn.addEventListener("click", function(){
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
 
-    const userType = document.getElementById("userType").value;
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirmPassword").value;
+    alert("Sikeres a regisztrálás");
 
-    if (password !== confirmPassword) {
-        alert("A két jelszó nem egyezik meg!");
-    } else {
-        alert("Regisztrálás sikeres!");
-        window.location.href = "fooldal.html";
-    }
-}
+    window.location.href = "fooldal.html";
+});
 
-document.getElementById("registrationForm").addEventListener("submit", validateForm);
-
-
-
-// Additional validation logic can be added here
-
-    // Assuming the validation is successful, redirect to "fooldal.html"
-
-
-
-
-    // If you want to prevent the default form submission, uncomment the line below
-    // return false;
-
-
-
-document.getElementById("userType").addEventListener("change", function () {
-    const hostFields = document.getElementById("hostFields");
-
-    if (this.value === "host") {
-        hostFields.style.display = "block";
-    } else {
-        hostFields.style.display = "none";
-    }
+prevBtnSec.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "0%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+});
+prevBtnThird.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "-25%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+});
+prevBtnFourth.addEventListener("click", function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = "-50%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
 });
